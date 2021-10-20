3f27f114e1965ab3f85c35127dd6933045d287e1
Costin1988 <92722839+Costin1988@users.noreply.github.com>
2021-10-20 11:37:19 +0300

    Add files via upload

---
+++ password-changed.php
@@ -0,0 +1,38 @@
+<?php require_once "controllerUserData.php"; ?>
+<?php
+if($_SESSION['info'] == false){
+    header('Location: login-user.php');  
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form login-form">
+            <?php 
+            if(isset($_SESSION['info'])){
+                ?>
+                <div class="alert alert-success text-center">
+                <?php echo $_SESSION['info']; ?>
+                </div>
+                <?php
+            }
+            ?>
+                <form action="login-user.php" method="POST">
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="login-now" value="Login Now">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
